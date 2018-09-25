2720e0fe11d8e38b021a68b9421466d162a8b2d5
Kevin Gates <kevinobamatheus@gmail.com>
2018-09-25 14:31:58 +0800

    update

---
+++ app/Http/Controllers/Api/ProductsController.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevingates
+ * Date: 9/25/18
+ * Time: 2:29 PM
+ */
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $products = Product::latest()->paginate(25);
+
+        return $products;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $product = Product::create($request->all());
+
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $product = Product::findOrFail($id);
+        $product->update($request->all());
+
+        return response()->json($product, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Product::destroy($id);
+
+        return response()->json(null, 204);
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: kevingates
+ * Date: 9/25/18
+ * Time: 2:29 PM
+ */
 
 namespace App\Models;
 
@@ -6,5 +12,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+
+    /**
+     * The database primary key value.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'description', 'image', 'thumbnail', 'price'];
 }

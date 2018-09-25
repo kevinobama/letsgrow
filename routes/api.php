50db2364f82c1ddb6ca3bc144c0ace0c87e61ec4
Kevin Gates <kevinobamatheus@gmail.com>
2018-09-25 15:03:26 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('products', 'Api\ProductsController');
+Route::resource('products', 'Api\ProductsController')->middleware('auth:api');;

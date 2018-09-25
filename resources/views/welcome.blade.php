152c9fd4c088872d96cad99cfe0b157a97dbd666
Kevin Gates <kevinobamatheus@gmail.com>
2018-09-25 14:06:13 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    kevin
                 </div>
 
                 <div class="links">
